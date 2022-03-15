4bd7da1328cf349a89e0e11c3cba6370b448e1c7
Zara A <fzali711@gmail.com>
2022-03-15 19:32:57 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+header.style.background = 'red'
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+let dogNames = document.querySelectorAll('.dog-name')
+for( i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+let dogPics = document.querySelectorAll('dog-image')
+for(let i = 0; i < dogPics.length; i++){
+    dogPics[i].style.transform.rotateX = ('180deg')
+}
+let footer = document.querySelector('.footer')
+    footer.style.color =('blue')
